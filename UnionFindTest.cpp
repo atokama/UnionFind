cc5f3dcaaea335312381aa2b6c5a2d906d3c8391
atoka <makrusan@gmail.com>
2020-05-23 15:17:19 +0300

    Use map instead of vector in representation

--- UnionFindTest.cpp
+++ UnionFindTest.cpp
@@ -6,19 +6,21 @@
 
 class UnionFind {
 public:
-    UnionFind(std::size_t size) {
-        arr_.resize(size);
-        for (std::size_t i{0}; i != arr_.size(); ++i) {
-            arr_[i] = i;
-        }
-        size_.assign(arr_.size(), 1);
+    using Size = std::size_t;
+
+    UnionFind() {
+        arr_[0] = 0;
+        size_[0] = 1;
     }
 
-    void unite(std::size_t a, std::size_t b) {
-        check(a, b);
+    void unite(Size a, Size b) {
+        check(a);
+        check(b);
         auto ra = root(a);
         auto rb = root(b);
-        if (size_[ra] < size_[rb]) {
+        if (ra == rb) {
+            return;
+        } else if (size_[ra] < size_[rb]) {
             arr_[ra] = arr_[rb];
             size_[rb] += size_[ra];
         } else {
@@ -27,30 +29,32 @@ public:
         }
     }
 
-    bool find(std::size_t a, std::size_t b) {
-        check(a, b);
+    bool find(Size a, Size b) {
+        check(a);
+        check(b);
         return root(a) == root(b);
     }
 
 private:
-    std::size_t root(std::size_t a) {
+    Size root(Size a) {
         while (arr_[a] != a) {
             a = arr_[a];
         }
         return a;
     }
 
-    void check(std::size_t a, std::size_t b) {
-        if (a < arr_.size() && b < arr_.size()) {
-            return;
-        } else throw std::runtime_error{"Out of range"};
+    void check(Size a) {
+        if (!arr_.count(a)) {
+            arr_[a] = a;
+            size_[a] = 1;
+        }
     }
 
-    std::vector<std::size_t> arr_, size_;
+    std::map<Size, Size> arr_, size_;
 };
 
 TEST_CASE("UnionFind", "[UnionFind]") {
-    UnionFind uf{8};
+    UnionFind uf{};
     REQUIRE(!uf.find(1, 2));
     uf.unite(1, 2);
     REQUIRE(uf.find(1, 2));
@@ -59,5 +63,6 @@ TEST_CASE("UnionFind", "[UnionFind]") {
     uf.unite(2, 3);
     REQUIRE(uf.find(1, 4));
 
-
+    uf.unite(1, 10);
+    REQUIRE(uf.find(1, 10));
 }
